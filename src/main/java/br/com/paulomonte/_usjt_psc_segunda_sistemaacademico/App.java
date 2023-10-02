d16ead7550a9c3a2210ae5223e5bc98b4b908724
paulo henrique <phlpaulohenriqueluiz@gmail.com>
2023-10-02 20:58:18 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/paulomonte/_usjt_psc_segunda_sistemaacademico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.paulomonte._usjt_psc_segunda_sistemaacademico;
+
+/**
+ *
+ * @author 82325475
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
